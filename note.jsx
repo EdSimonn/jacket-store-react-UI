821461490e16e3cd9bf2b883b8665ce214422304
EdSimonn <dami25edusi@gmail.com>
2022-01-14 05:48:05 -0800

    First commit

---
+++ note.jsx
@@ -0,0 +1,15 @@
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+`;
---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/7WLQFCV/fococlipping-20211223-73935-removebg-preview.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/p21C5r2/fococlipping-20211223-83816.png",
+    title: "BLACK FRIDAY COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/Hp1Tyyz/cole-wyland-O1z2bk8-Yo-Co-unsplash-removebg-preview.png",
+    title: "COLORFUL COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/0fswCQC/pexels-mohamedamine-abbas-6968370.jpg",
+    title: "MEN!",
+    cat: "men"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/7GkqGP4/thomas-le-HLf8-Nd-Gc-I0g-unsplash.jpg",
+    title: "WOMEN",
+    cat: "women"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/H2LwPBH/Adrian-dascal-1-QOs-JGb-NIgk-unsplash.jpg",
+    title: "OTHERS",
+    cat: "others"
+  },
+];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://i.ibb.co/MGTSj2Z/slide-4-removebg-preview.png",
+    },
+    {
+      id:2,
+      img:"https://i.ibb.co/2n5gy4g/1-removebg-preview.png",
+    },
+    {
+      id:3,
+      img:"https://i.ibb.co/jRhZQJy/olga-doganoc-kex2-O4pk-W-0-unsplash-removebg-preview.png",
+    },
+    {
+      id:4,
+      img:"https://i.ibb.co/jRhZQJy/olga-doganoc-kex2-O4pk-W-0-unsplash-removebg-preview.png",
+    },
+    {
+      id:5,
+      img:"https://i.ibb.co/FmXStQ4/jacket-PNG8056-removebg-preview.png",
+    },
+  ]
